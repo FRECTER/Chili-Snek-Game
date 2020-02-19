73c74b3ed0375987dffcf39294dcf265fca5e6e0
FRECTER Don <poyertytheminecrafter@gmail.com>
2020-02-19 23:11:33 +0800

    added obstacle spawning and checking mechanic

--- Engine/Board.cpp
+++ Engine/Board.cpp
@@ -1,4 +1,6 @@
 #include "Board.h"
+#include "Snake.h"
+#include "Goal.h"
 #include <assert.h>
 
 Board::Board(Graphics & gfx)
@@ -11,7 +13,7 @@ void Board::DrawCell(const Location & loc, Color c) {
 	assert(loc.x < width);
 	assert(loc.y >= 0);
 	assert(loc.y < height);
-	gfx.DrawRectDim(loc.x * cellDim + (Graphics::ScreenWidth - width * cellDim) / 2 + 1, loc.y*cellDim + (Graphics::ScreenHeight - height * cellDim) / 2 + 1, cellDim - 1, cellDim - 1, c);
+	gfx.DrawRectDim(loc.x * cellDim + (Graphics::ScreenWidth - width * cellDim) / 2 + 1, loc.y * cellDim + (Graphics::ScreenHeight - height * cellDim) / 2 + 1, cellDim - 1, cellDim - 1, c);
 }
 
 int Board::GetWidth() const {
@@ -28,4 +30,19 @@ int Board::GetDim() const {
 
 bool Board::Inside(const Location& loc) const {
 	return loc.x >= 0 && loc.x < width && loc.y >= 0 && loc.y < height;
+}
+
+bool Board::CheckObs(const Location& loc) const {
+	return haveObs[loc.y * height + loc.x];
+}
+
+void Board::SpawnObs(std::mt19937& rng, const Location& loc, const Snake& snake, const Goal& goal) {
+	std::uniform_int_distribution<int> XDist(0, width - 1);
+	std::uniform_int_distribution<int> YDist(0, height - 1);
+	Location newLoc;
+	do {
+		newLoc.x = XDist(rng);
+		newLoc.y = YDist(rng);
+	} while (snake.InTile(newLoc) || CheckObs(newLoc) || goal.getLocation() == newLoc);
+	haveObs[newLoc.y * height + newLoc.x] = true;
 }
--- Engine/Board.h
+++ Engine/Board.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Graphics.h"
 #include "Location.h"
+#include <random>
 
 class Board {
 public:
@@ -10,9 +11,13 @@ public:
 	int GetHeight() const;
 	int GetDim() const;
 	bool Inside(const Location& loc) const;
+	bool CheckObs(const Location& loc) const;
+	void SpawnObs(std::mt19937& rng, const Location& loc, const class Snake& snake, const class Goal& goal);
+
 private:
 	static constexpr int cellDim = 10;
 	static constexpr int width = 70;
 	static constexpr int height = 50;
 	Graphics& gfx;
+	bool haveObs[width * height] = { false };
 };
